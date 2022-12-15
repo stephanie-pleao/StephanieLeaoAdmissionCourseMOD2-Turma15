b5643480fdf5c7cc39736fd70c3b1217e0ac5415
Stephanie Leao <stephanie.patricia.leao@hotmail.com>
2022-12-14 21:39:50 -0300

    Fazer menu

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -20,8 +20,10 @@ class ObstacleManager:
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                pygame.time.delay(100)
+                pygame.time.delay(500)
+                
                 game.playing = False
+                game.death_count += 1
                 break
 
 
@@ -29,3 +31,6 @@ class ObstacleManager:
         for obstacle in self.obstacles:
             obstacle.draw(screen)
             
+    def reset_obstacles(self):
+        self.obstacles = []
+
--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from dino_runner.components.game import Game
 
 if __name__ == "__main__":
     game = Game()
-    game.run()
+    game.execute()
+    
     #vai rodar nosso jogo
     ##executar esse - rodar
 
     #tarefa adicionar os cactus grandes
     #tenta adiocinar so uma imagem qnd aparecer a logica ta ok ai vai na llogica pra fazer as perninhas mudandos
     # adicoinar os passaros(o cacto aparece na tela junto c o chao o passado tem que muda)
+    #Fazer o score
+    #Fazer menu 
+
+    #Arrumar tela e Score
+    #Quando morrer p/ aparecer uma mensagem p reiciniar
+    #dentro do menu de quandoe le morre colocar p aparecer as insformacoes
+    #e p resetar a informacao
+    #tirar a repeticao do texto
